28f70286f5ef5f7ed0cb95f28829cb7c5a222100
diedmon <62182463+diedmon@users.noreply.github.com>
2023-12-18 01:56:51 +0800

    Update test_empty_stream.py

--- tests/test_empty_stream.py
+++ tests/test_empty_stream.py
@@ -2,7 +2,7 @@ import py7zr
 import pytest
 
 @pytest.mark.empty_stream
-def test_empty_stream(self):
+def test_empty_stream():
   try:
     with py7zr.SevenZipFile(file="test.7z",mode="w") as archive:
       archive.writestr(data="",arcname="empty.txt")
