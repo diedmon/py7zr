a484e4fa38703a0f260518da17e37235302b216d
diedmon <62182463+diedmon@users.noreply.github.com>
2023-12-18 01:12:52 +0800

    Update test_empty_stream.py

--- tests/test_empty_stream.py
+++ tests/test_empty_stream.py
@@ -7,4 +7,4 @@ def test_empty_stream(self):
     with py7zr.SevenZipFile(file="test.7z",mode="w") as archive:
       archive.writestr(data="",arcname="empty.txt")
   except:
-    self.fail("Exception raised")
+    pytest.fail("Exception raised")
